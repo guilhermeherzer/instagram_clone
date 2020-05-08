41ba839251313bd2758930a321e73348881e8409
Guilherme Rosa <guilherme@live.com>
2020-05-08 14:20:27 -0300

    codigo melhorado, informações auth->user passadas pelo service user

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -8,62 +8,55 @@ import { NativeStorage } from '@ionic-native/native-storage/ngx';
 
 import { PostService } from './../api/post.service';
 
+import { UserService } from './../api/user.service';
+
 @Component({
   	selector: 'app-tab2',
   	templateUrl: 'tab2.page.html',
   	styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-	private url = 'http://192.168.0.127/';
+	private url = 'http://192.168.0.127/'
+	private	auth: any
+	private users = []
+	private texto: string
 
-	private	myId: string;
-
-	private users = [];
-
-	private texto: string;
-
-  	constructor(public router: Router,
-				public toastCtrl: ToastController,
+  	constructor(private router: Router,
+				private toastCtrl: ToastController,
 				private storage: NativeStorage,
-				private postService: PostService) {
-  		this.texto = '';
+				private postService: PostService,
+				private user: UserService) {
+		this.user.getAuth()
+			.then(result => { 
+				this.auth = result 
+			})
+  		this.texto = ''
   	}
 
 	ngOnInit(){
-  		this.loadData('data');
 	}
 
 	async showToast(message: string) {
-		const toast = await this.toastCtrl.create({message, duration: 2000, position: 'bottom' });
-		toast.present();
+		const toast = await this.toastCtrl.create({message, duration: 2000, position: 'bottom' })
+		toast.present()
   	}
 
   	buscarPerfil(){
 		this.postService.buscar(this.texto)
 			.then((result: any) => {
-				this.users = result.responseData['users'];
+				this.users = result.responseData['users']
 			})
 			.catch((error: any) => {
-				this.showToast('Erro ao carregar o feed. Erro:' + error.error);
+				this.showToast('Erro ao carregar o feed. Erro:' + error.error)
 			})
   	}
 
   	verPerfil(id: string){
-  		if(id == this.myId){
-  			this.router.navigate(['/tabs/tab5']);
+  		if(id == this.auth.id){
+  			this.router.navigate(['/tabs/tab5'])
   		}else{
-  			this.router.navigate(['/tabs/perfil', id]);
+  			this.router.navigate(['/tabs/perfil', id])
   		}
-  	}
-
-  	async loadData(name: string){
-		this.storage.getItem(name)
-			.then(data => {
-				this.myId = data.id;
-			})
-			.catch(error => {
-				this.showToast('Erro ao carregar o feed. Erro:' + error.error);
-			});
   	}
 }
